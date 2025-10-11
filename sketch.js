32e5cd9144d97e76c81503fa12fa5874ac2173a8
matildecurino <matilde.curino0509@gmail.com>
2025-10-11 23:34:52 +0200

    [sketch] Mean0_disegno palme
    1.Sistemata canva
    2.Mean colonna 0

--- sketch.js
+++ sketch.js
@@ -1,15 +1,39 @@
+let table; //tabella dataset
+let validRows = []; //righe valide che rispettano le regole
+let palm; //img della palma
 
-  let table;
-  let validRows = [];
+//variabili per le colonne e operazioni
+let col0 = [];
+let col1 = [];
+let col2 = [];
+let col3 = []; 
+let col4 = [];
+let mean0; 
+let std1;
+let mode2;
+let median3;
+let mean4;
+let std4;
 
-  function preload() {
-    table = loadTable("matildecurino/dataset.csv", "csv", "header");
-  }
+//variabili per mean0
+let minvalMean0;
+let maxvalMean0;
+let marginX;
+let spacing;
+let baseYMean0;
+let maxPalmHeight;
 
-  function setup() {
-    createCanvas(900, 600);
-    textSize(16);
-    noLoop();
+function preload() {
+  table = loadTable("matildecurino/dataset.csv", "csv", "header");
+
+  //media column0
+  palm = loadImage("palm.png");
+}
+
+function setup() {
+  createCanvas(1000, 700);
+  textSize(14);
+  noLoop();
 
   //ciclo per la selezione delle righe che rispettano le rules assegnate
   for (let r = 0; r < table.getRowCount(); r++) {
@@ -41,31 +65,93 @@
   }
 
   //variabili per chiamare le colonne
-  let col0 = validRows.map(r => r.getNum("column0"));
-  let col1 = validRows.map(r => r.getNum("column1"));
-  let col2 = validRows.map(r => r.getNum("column2"));
-  let col3 = validRows.map(r => r.getNum("column3"));
-  let col4 = validRows.map(r => r.getNum("column4"));
+  col0 = validRows.map(r => r.getNum("column0"));
+  col1 = validRows.map(r => r.getNum("column1"));
+  col2 = validRows.map(r => r.getNum("column2"));
+  col3 = validRows.map(r => r.getNum("column3"));
+  col4 = validRows.map(r => r.getNum("column4"));
 
-  //variabili per chiamare le operazioni (media,moda,mediana,standard deviation)
+  //variabili per chiamare il calcolo delle operazioni 
+  //(media,moda,mediana,standard deviation)
   //relative alle specifiche colonne
-  let mean0 = mean(col0);
-  let std1 = std(col1);
-  let mode2 = mode(col2);
-  let median3 = median(col3);
-  let mean4 = mean(col4);
-  let std4 = std(col4);
+  mean0 = mean(col0);
+  std1 = std(col1);
+  mode2 = mode(col2);
+  median3 = median(col3);
+  mean4 = mean(col4);
+  std4 = std(col4);
 
-  background(245);
-  fill(0);
-  textAlign(LEFT);
-  text(`Mean (column0): ${mean0.toFixed(2)}`, 50, 50);
-  text(`Std (column1): ${std1.toFixed(2)}`, 50, 75);
-  text(`Mode (column2): ${mode2}`, 50, 100);
-  text(`Median (column3): ${median3}`, 50, 125);
-  text(`Mean (column4): ${mean4.toFixed(2)}, Std: ${std4.toFixed(2)}`, 50, 150);
+  textFont('Courier New');
 
+  //valore minimo e massimo per scalare l'altezza delle palme in modo 
+  //proporzionale ai valori numerici 
+  minvalMean0 = Math.min(...col0);
+  maxvalMean0 = Math.max(...col0);
+
+  //margini e spaziatura
+  marginX = 80;
+  spacing = (1000 - 2 * marginX) / (col0.length - 0.5);
+  baseYMean0 = 500 - 100; // linea del terreno massimo
+  maxPalmHeight = 300 ; // altezza massima della palma
 }
 
 
+function draw() {
+  //Scaling dei valori per la dimensione della palma
+  function scalevalueMean0(valMean0) {
+    return map(valMean0, minvalMean0, maxvalMean0, 50, maxPalmHeight);
+  }
 
+  //Scaling della media lungo l'asse delle y
+  //Più grande è la media + alto è il confine tra sabbia e cielo
+  let meanYMean0 = map(mean0, minvalMean0, maxvalMean0, baseYMean0 - maxPalmHeight, baseYMean0);
+
+  noStroke();
+  //Cielo
+  fill(135, 206, 250); //azzurro
+  rect(0, 0, width, meanYMean0); 
+  //dalla coordinata 0 0, largo come tutta la canva e alto fino alla linea della media
+
+  //Sabbia
+  fill(222, 184, 135); //marrone chiaro
+  rect(0, meanYMean0, width, 500 - meanYMean0);
+
+  //Media
+  // stroke(0);
+  // strokeWeight(2);
+  // line(0, meanYMean0, width, meanYMean0);
+
+  noStroke();
+  fill(0);
+  textSize(12);
+  text(`${mean0.toFixed(2)}`, 50, meanYMean0);
+
+  //Disegno delle palme
+  imageMode(CENTER);
+  textAlign(CENTER);
+  noStroke();
+  fill(0);
+
+  for (let i = 0; i < col0.length; i++) {
+    let valMean0 = col0[i];
+    let palmHeight = scalevalueMean0(valMean0);
+    let xMean0 = marginX + i * spacing;
+    let yMean0 = baseYMean0 - palmHeight / 2; // posizionamento verticale
+
+    //palma
+    image(palm, xMean0, yMean0, palmHeight * 0.6, palmHeight);
+
+    //valori numerici sotto la palma
+    textSize(10);
+    text(`(${valMean0})`, xMean0, baseYMean0 + 20);
+  }
+
+  //titolo e spiegazione
+  noStroke();
+  fill(0);
+  textSize(25);
+  textAlign(LEFT);
+  text(`Mean column0 = ${mean0.toFixed(2)}`, 50, 50);
+  textSize(14);
+  text("Linea di separazione tra la sabbia e il cielo ", 50, 70);
+}
